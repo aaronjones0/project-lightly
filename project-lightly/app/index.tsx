f543b878ecbc0d07cb2ec4fb56fe83031ef78b3d
Aaron <126199236+aaronjones0@users.noreply.github.com>
2025-03-25 21:14:08 -0400

    Install and configure NativeWind

--- project-lightly/app/index.tsx
+++ project-lightly/app/index.tsx
@@ -3,14 +3,15 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
+      className='bg-stone-100 text-stone-700'
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>nudge</Text>
-      <Text>Welcome, friend.</Text>
+      <Text className='text-6xl font-semibold text-stone-500'>nudge</Text>
+      <Text>{'Hi :)'}</Text>
     </View>
   );
 }
