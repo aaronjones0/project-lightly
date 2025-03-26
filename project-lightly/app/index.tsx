e54edfb888ce334af651a96ba767893bbc954de2
Aaron <126199236+aaronjones0@users.noreply.github.com>
2025-03-25 21:39:47 -0400

    Install and configure expo-google-fonts

--- project-lightly/app/index.tsx
+++ project-lightly/app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Platform, Text, View } from 'react-native';
 
 export default function Index() {
   return (
@@ -6,12 +6,30 @@ export default function Index() {
       className='bg-stone-100 text-stone-700'
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
-      <Text className='text-6xl font-semibold text-stone-500'>nudge</Text>
-      <Text>{'Hi :)'}</Text>
+      <Text
+        className='text-6xl font-semibold text-stone-500'
+        style={{
+          fontFamily: Platform.select({
+            android: 'Inter_400Regular',
+            ios: 'Inter-Regular',
+            // android: 'RobotoSerif_100Thin',
+            // ios: 'RobotoSerif-Thin',
+          }),
+        }}
+      >
+        nudge
+      </Text>
+      <Text style={{
+        fontFamily: Platform.select({
+          android: 'Inter_400Regular',
+          ios: 'Inter-Regular',
+        }),
+      }}>{'Hi :)'}</Text>
     </View>
   );
 }
+
